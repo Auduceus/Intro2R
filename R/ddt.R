e759b4bf598f2d582dc4fb135146fc44ec66f8e9
Auduceus <jackisswag77@gmail.com>
2025-12-03 05:02:01 -0600

    add some data

---
+++ R/ddt.R
@@ -0,0 +1,15 @@
+#' DDT data set
+#'
+#' A data set of fish collected in the tennessee river
+#'
+#' @format A data frame with 144 rows and 5 variables:
+#' \describe{
+#'   \item{LENGTH}{length in cm}
+#'   \item{WEIGHT}{weight of the fish, in gms}
+#'   \item{MILE}{mile where collected}
+#'   \item{RIVER}{river name}
+#'   \item{SPECIES}{fish species}
+#'   \item{DDT}{DDT concentration}
+#' }
+#' @source \url{https://www.crcpress.com/Statistics-for-Engineering-and-the-Sciences/Mendenhall-Sincich/p/book/9781498728850}
+"ddt"
